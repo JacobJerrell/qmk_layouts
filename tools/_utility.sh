cf646c25bc2199ed8aa5c9a70cae304b2435f223
Jacob Jerrell <jacob.jerrell@gmail.com>
2020-08-23 23:01:09 -0500

    More progress on v2 script

--- tools/_utility.sh
+++ tools/_utility.sh
@@ -10,15 +10,6 @@
 # Message Functions #
 #####################
 
-_help_msg()
-{
-    printf 'Usage: qmkws [-c] [-d] [-v] [keyboard1, keyboard2, ...]\n'
-    printf '\t -c\t\t\tCompiles the given keymaps or all of them\n'
-    printf '\t -d\t\t\tPrints debug messages\n'
-    printf '\t -v\t\t\tPasses the verbose command to system commands where applicable\n'
-    printf '\t [keyboard...]\t\t\tPerforms actions for only the provided keyboards\n'
-}
-
 _send_msg()
 {
     if [[ "$1" != "-"* ]]; then
@@ -34,12 +25,10 @@ _send_msg()
     case "${first_arg}" in
          -m|--msg) 
             printf '%s\n' "${message}"
-            return 0
             ;;
         -f|--fatal)
             _send_msg -h " !! Fatal Error !! "
             _send_msg -m "ERROR :: ${message}"
-            return 1
             ;;
         -h|--header)
             header_length=$(( ${#message} + 4 ))
--- tools/qmk_workspace.sh
+++ tools/qmk_workspace.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-
 source _utility.sh
+source qmkws.cfg
+
+_send_msg -h "QMK Workspace Utility"
+
+_help_msg()
+{
+    _send_msg -m "Usage: qmkws [-c] [-d] [-v] [keyboard1, keyboard2, ...]"
+    _send_msg -m "\t -c\t\t\tCompiles the given keymaps or all of them"
+    _send_msg -m "\t -d\t\t\tPrints debug messages"
+    _send_msg -m "\t -v\t\t\tPasses the verbose command to system commands where applicable"
+    _send_msg -m "\t [keyboard...]\t\t\tPerforms actions for only the provided keyboards"
+}
+
+_debug_msg()
+{
+    if [[ "$debug" == "true" ]]; then
+        _send_msg -d "$*"
+    fi
+}
 
 ####################
 # Argument Parsing #
@@ -28,26 +46,40 @@ do
             shift
             ;;
         -*) 
-            _send_msg -f "Unknown Argument: $arg"
-            shift
+            _help_msg
+            return 1
             ;;
     esac
 done
 
-_debug_msg()
-{
-    if [[ "$debug" == "true" ]]; then
-        _send_msg -d $@
-    fi
-}
-
-_send_msg -h "QMK Workspace Utility"
 _debug_msg "Debug Messages Enabled"
 _debug_msg "Passing $# unparsed arguments"
 for arg
 do
     if [[ ! -z "${arg[i]}" ]]; then
-        _debug_msg "Argument #$i: "${arg[i]}""
+        _debug_msg "Argument : \"${arg[i]}\""
     fi
 done
 
+#####################
+# Environment Check #
+#####################
+
+_qmk_setup_msg()
+{
+    _send_msg -f "Firmware does not appear to exist at ${qmk_firmware}." 
+    _send_msg -m "- Check your configuration by editing the ``qmkws.sh`` file"
+    _send_msg -m "- Otherwise, you may need to clone and setup QMK first. Link: https://docs.qmk.fm/#/newbs_getting_started"
+}
+
+_config_check()
+{
+    test_file="quantum/api.c"
+    if [[ ! -r "${qmk_firmware}/${test_file}" ]]; then
+        debug_msg fatal "Firmware does not appear to exist at ${qmk_firmware}.
+        - Check your configuration by editing the ``qmkws.sh`` file
+        - Otherwise, you may need to clone and setup QMK first. Link: https://docs.qmk.fm/#/newbs_getting_started"
+    else
+        debug_msg info "Firmware Path: ${qmk_firmware}"
+    fi
+}
